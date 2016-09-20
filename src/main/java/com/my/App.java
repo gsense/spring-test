51c762277eb402da9b7377a14fa8afdd64b6446e
gsense <852872207@qq.com>
2016-09-20 15:45:52 +0800

    test

--- src/main/java/com/my/App.java
+++ src/main/java/com/my/App.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTestApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringTestApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 }
